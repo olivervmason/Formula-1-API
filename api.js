1b697daa85fb60c4da4ce28091444ad7c2291f07
Oliver Mason <omason@protonmail.com>
2020-06-20 23:16:12 +1000

    Top 10 complete

--- api.js
+++ api.js
@@ -73,15 +73,73 @@ searchBtn.addEventListener("click", async () => {
     const p3constructor = result.MRData.RaceTable.Races[0].Results[2].Constructor.name
     const p3nationality = result.MRData.RaceTable.Races[0].Results[2].Driver.nationality
 
-    third.innerHTML = `Third place: ${p3name} ${p3surname} (${p3nationality}) - ${p3constructor}`
+    third.innerHTML = `3rd place: ${p3name} ${p3surname} (${p3nationality}) - ${p3constructor}`
 
-    // Other honours
+    // P4
 
-    // const flap = 
+    const p4name = result.MRData.RaceTable.Races[0].Results[3].Driver.givenName
+    const p4surname = result.MRData.RaceTable.Races[0].Results[3].Driver.familyName
+    const p4constructor = result.MRData.RaceTable.Races[0].Results[3].Constructor.name
+    const p4nationality = result.MRData.RaceTable.Races[0].Results[3].Driver.nationality
+
+    fourth.innerHTML = `4th place: ${p4name} ${p4surname} (${p4nationality}) - ${p4constructor}`
+
+    // P5
+
+    const p5name = result.MRData.RaceTable.Races[0].Results[4].Driver.givenName
+    const p5surname = result.MRData.RaceTable.Races[0].Results[4].Driver.familyName
+    const p5constructor = result.MRData.RaceTable.Races[0].Results[4].Constructor.name
+    const p5nationality = result.MRData.RaceTable.Races[0].Results[4].Driver.nationality
+
+    fifth.innerHTML = `5th place: ${p5name} ${p5surname} (${p5nationality}) - ${p5constructor}`
+
+    // P6
+    const p6name = result.MRData.RaceTable.Races[0].Results[5].Driver.givenName
+    const p6surname = result.MRData.RaceTable.Races[0].Results[5].Driver.familyName
+    const p6constructor = result.MRData.RaceTable.Races[0].Results[5].Constructor.name
+    const p6nationality = result.MRData.RaceTable.Races[0].Results[5].Driver.nationality
+
+    sixth.innerHTML = `6th place: ${p6name} ${p6surname} (${p6nationality}) - ${p6constructor}`     
+
+    // P7
+    const p7name = result.MRData.RaceTable.Races[0].Results[6].Driver.givenName
+    const p7surname = result.MRData.RaceTable.Races[0].Results[6].Driver.familyName
+    const p7constructor = result.MRData.RaceTable.Races[0].Results[6].Constructor.name
+    const p7nationality = result.MRData.RaceTable.Races[0].Results[6].Driver.nationality
+
+    seventh.innerHTML = `7th place: ${p7name} ${p7surname} (${p7nationality}) - ${p7constructor}`
+
+    // P8
+    const p8name = result.MRData.RaceTable.Races[0].Results[7].Driver.givenName
+    const p8surname = result.MRData.RaceTable.Races[0].Results[7].Driver.familyName
+    const p8constructor = result.MRData.RaceTable.Races[0].Results[7].Constructor.name
+    const p8nationality = result.MRData.RaceTable.Races[0].Results[7].Driver.nationality
+
+    eigth.innerHTML = `8th place: ${p8name} ${p8surname} (${p8nationality}) - ${p8constructor}`
+
+    // P9
+    const p9name = result.MRData.RaceTable.Races[0].Results[8].Driver.givenName
+    const p9surname = result.MRData.RaceTable.Races[0].Results[8].Driver.familyName
+    const p9constructor = result.MRData.RaceTable.Races[0].Results[8].Constructor.name
+    const p9nationality = result.MRData.RaceTable.Races[0].Results[8].Driver.nationality
+
+    ninth.innerHTML = `9th place: ${p9name} ${p9surname} (${p9nationality}) - ${p9constructor}`
+
+    // P10
+    const p10name = result.MRData.RaceTable.Races[0].Results[9].Driver.givenName
+    const p10surname = result.MRData.RaceTable.Races[0].Results[9].Driver.familyName
+    const p10constructor = result.MRData.RaceTable.Races[0].Results[9].Constructor.name
+    const p10nationality = result.MRData.RaceTable.Races[0].Results[9].Driver.nationality
+
+    tenth.innerHTML = `10th place: ${p10name} ${p10surname} (${p10nationality}) - ${p10constructor}`
+
+    // Pole Position
+
     const poleDriverName = qualy.MRData.RaceTable.Races[0].QualifyingResults[0].Driver.givenName
     const poleDriverSurname = qualy.MRData.RaceTable.Races[0].QualifyingResults[0].Driver.familyName 
     const poleDriverNationalty = qualy.MRData.RaceTable.Races[0].QualifyingResults[0].Driver.nationality 
     const poleDriverTeam = qualy.MRData.RaceTable.Races[0].QualifyingResults[0].Constructor.name
 
     pole.innerHTML = `Pole position: ${poleDriverName} ${poleDriverSurname} (${poleDriverNationalty}) - ${poleDriverTeam} `
+
 })
